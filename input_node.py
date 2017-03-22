7100d16740f71883461b6dd30484f6186d51304b
JacobPimental <jacob16682@gmail.com>
2017-03-22 16:43:55 -0400

    Made some tweaks so it works with the new classes

--- input_node.py
+++ input_node.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3 
 
 from synapses import *
+from hidden_node import *
+import math
 
 class InputNode:
 
     synapses = []
-    data = 0
+    data = 0.0
 
     #sets up the data in our input node
     #parameters:
@@ -27,13 +29,22 @@ class InputNode:
         return "Input Node:\n\tData: " + str(self.data) + "\n\tSynapses: " + str(self.synapses)
 
 
+def act_func( data ):
+    print( "Using activation function on " + str( data ) )
+    ans = float( 1/(1 + math.pow(math.e, -data) ) )
+    print( "Got: " + str(ans) )
+    return ans
+
 if __name__ == "__main__":
     mysynapses = []
-    mysynapses.append( Synapse( 2 ) )
+    mysynapses.append( Synapse( 0.02 ) )
+    mysynapses.append( Synapse( 0.03 ) )
     
-    mysynapses.append( Synapse( 3 ) )
-    
-    input_node = InputNode( 52, mysynapses )
+    mysynapses2 = []
+ 
+    input_node = InputNode( 1, mysynapses )
+    hidden_node = HiddenNode( mysynapses, mysynapses2, act_func )
+    input_node.calculate()
     
     print( input_node )
 
