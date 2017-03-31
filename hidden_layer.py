30df0efe76fc52391d23d719067e8d5f4b99bfa3
JacobPimental <jacob16682@gmail.com>
2017-03-31 17:30:55 -0400

    Started work on backpropogation

--- hidden_layer.py
+++ hidden_layer.py
@@ -5,14 +5,16 @@ from synapses import *
 import random
 
 class HiddenLayer:
-    
+   
+    weights = []
     myNodes = []
     act_func = None
     numNodes = 0
     nextLayer = None
     endNode = None
 
-    def __init__(self, numNodes, act_func, endNode, nextLayer):
+    def __init__(self, numNodes, act_func, endNode, nextLayer, weights=[]):
+        self.weights = weights[:]
         self.numNodes = numNodes
         self.endNode = endNode
         self.act_func = act_func
@@ -21,20 +23,27 @@ class HiddenLayer:
 
     def init_nodes(self):
         for i in range( 0, self.numNodes ):
-            self.myNodes.append( HiddenNode( self.formulate_synapses(), self.act_func) )
+            self.myNodes.append( HiddenNode( self.formulate_synapses(i), self.act_func) )
 
     def send_data(self):
         for n in self.myNodes:
             n.pass_through()
 
-    def formulate_synapses(self):
+    def formulate_synapses(self, curNode):
         lst = []
+        print( str( self.weights ) + " " + str( curNode ) )
         if not self.nextLayer == None:
-            for n in self.nextLayer.myNodes:
-                lst.append( Synapse( random.random(), n ) )
-
+            for n in range(0, len(self.nextLayer.myNodes)):
+                if len( self.weights ) == 0:
+                    lst.append( Synapse( random.random(), self.nextLayer.myNodes[n] ) )
+                else:
+                    lst.append( Synapse( self.weights[curNode][n], self.nextLayer.myNodes[n] ) )
+        
         else:
-            lst.append( Synapse( random.random(), self.endNode ) )
+            if len( self.weights ) == 0:
+                lst.append( Synapse( random.random(), self.endNode ) )
+            else:
+                lst.append( Synapse( self.weights[curNode][0], self.endNode ) )
 
         return lst
 
